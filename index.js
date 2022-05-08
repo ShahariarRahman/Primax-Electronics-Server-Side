90d2f9310cf1c7f18461dd1bcddfde1e47b7f7fe
ShahariarRahman <shahariarrahman98@gmail.com>
2022-05-08 17:19:24 +0600

    jwt token created

--- index.js
+++ index.js
@@ -11,6 +11,34 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dwqxc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        await client.connect();
+        const inventoryCollection = client.db('products').collection('inventory');
+
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ token });
+        });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('server is running');
 });
