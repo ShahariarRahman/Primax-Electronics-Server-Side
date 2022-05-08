9dabd2605673e444d53a4e2e2685f4d940bc9dcc
ShahariarRahman <shahariarrahman98@gmail.com>
2022-05-08 17:16:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('server is running');
+});
+
+app.listen(port, () => console.log('Listening port', port));
